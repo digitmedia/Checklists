b881cb996960824f048bc32174179f02d06638c8
Luc Peeters <luc@digitmedia.be>
2015-05-26 21:05:08 +0200

    Pre-Array version
    Bug: checkmarks don’t work anymore after creating a separate method
    configureCheckmarkForCell

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Luc Peeters on 26/05/15.
+//  Copyright (c) 2015 Digitmedia. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -16,6 +16,13 @@ class ChecklistViewController: UITableViewController {
     var row2text = "Learn iOS development"
     var row3text = "Sailing practice"
     var row4text = "Take a break"
+    
+    // Add instance variables for the state of the checkmark for each row
+    var row0checked = false
+    var row1checked = false
+    var row2checked = false
+    var row3checked = false
+    var row4checked = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,25 +54,54 @@ class ChecklistViewController: UITableViewController {
             label.text = row4text
         }
         
+        configureCheckmarkForCell(cell, indexPath: indexPath)
+        
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
             
-            // Tapping/Selecting a cell toggles the checkmark
             if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-                if cell.accessoryType == .None {
+               // Tapping/Selecting a cell toggles the checkmark for that indexPath
+               if indexPath.row == 0 {
+                 row0checked = !row0checked
+               } else if indexPath.row == 1 {
+                 row1checked = !row1checked
+              } else if indexPath.row == 2 {
+                 row2checked = !row2checked
+              } else if indexPath.row == 3 {
+                 row3checked = !row3checked
+              } else if indexPath.row == 4 {
+                 row4checked = !row4checked
+              }
+            
+              configureCheckmarkForCell(cell, indexPath: indexPath)
+            }
+    
+            // Cell does not remain selected (grey) when tapped
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            }
+    
+    func configureCheckmarkForCell(cell: UITableViewCell, indexPath: NSIndexPath) {
+                
+                var isChecked = false
+                
+                if indexPath.row == 0 {
+                    isChecked = row0checked
+                } else if indexPath == 1 {
+                    isChecked = row1checked
+                } else if indexPath == 2 {
+                    isChecked = row2checked
+                } else if indexPath == 3 {
+                    isChecked = row3checked
+                } else if indexPath == 4 {
+                    isChecked = row4checked
+                }
+                
+                if isChecked {
                     cell.accessoryType = .Checkmark
                 } else {
                     cell.accessoryType = .None
                 }
-            }
-            
-            // Cell does not remain selected (grey) when tapped
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-
     }
-    
-
 }
-
